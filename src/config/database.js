b9d5c3e88cb1beca84d6fe89f09dfe5eada8ab05
joaoenrique <jebsantosalves@gmail.com>
2024-10-10 15:05:20 -0300

    fix: voltar para mysql

--- src/config/database.js
+++ src/config/database.js
@@ -2,36 +2,15 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 const dbConfig = {
-    dialect: 'postgres',
+    dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE, 
     define: {
         timestamps: true,
-        underscored: true, // Usar snake_case nos nomes de colunas e tabelas
+        underscored: true, // Define que os nomes de colunas e tabelas serão em snake_case
     },
-    dialectOptions: {
-        ssl: process.env.DB_SSL === 'true' ? {
-        require: true,
-        rejectUnauthorized: false, // Ajuste se necessário
-        } : false
-    },
-    logging: false, // Desativar logs SQL
 }
-
-// VERSÃO MYSQL
-// const dbConfig = {
-//     dialect: process.env.DB_DIALECT,
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE, 
-//     define: {
-//         timestamps: true,
-//         underscored: true, // Define que os nomes de colunas e tabelas serão em snake_case
-//     },
-// }
 
 module.exports = dbConfig;
