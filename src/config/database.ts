a623b44651eccf7b9b69a8b5c2e8ae22982cfc34
joaoenrique <jebsantosalves@gmail.com>
2024-10-10 16:26:28 -0300

    fix: conexão com mysql e postgres

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,8 +8,21 @@ const isPostgres = process.env.DB_DIALECT === 'postgres';
 
 const dbConfig: any = isPostgres
     ? {
-          url: process.env.DATABASE_URL, // URL para conexão com o PostgreSQL
-          dialect: 'postgres',
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        define: {
+            timestamps: true,
+            underscored: true, // Define que os nomes de colunas e tabelas serão em snake_case
+        },
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true, // Requer SSL
+                rejectUnauthorized: false, // Define para false se o certificado não for confiável, mas cuidado com isso em produção
+            },
+        },
       }
     : {
           host: process.env.DB_HOST,
