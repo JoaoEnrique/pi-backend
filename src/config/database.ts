650c4d73301c97288803ce74e642b03f6918d352
joaoenrique <jebsantosalves@gmail.com>
2024-10-10 16:09:20 -0300

    feat: banco de dados mysql e postgree

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,18 +1,26 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import { Options } from 'sequelize';
 
 dotenv.config();
 
-const dbConfig: Options = {
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE, 
-    define: {
-        timestamps: true,
-        underscored: true, // Define que os nomes de colunas e tabelas serão em snake_case
-    },
-    dialect: 'mysql',
-}
+// Determina o tipo de banco de dados a ser usado
+const isPostgres = process.env.DB_DIALECT === 'postgres';
+
+const dbConfig: any = isPostgres
+    ? {
+          url: process.env.DATABASE_URL, // URL para conexão com o PostgreSQL
+          dialect: 'postgres',
+      }
+    : {
+          host: process.env.DB_HOST,
+          username: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          define: {
+              timestamps: true,
+              underscored: true, // Define que os nomes de colunas e tabelas serão em snake_case
+          },
+          dialect: 'mysql',
+      };
 
 export = dbConfig;
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
-import dbConfig from '../config/database';
+import dbConfig from '../config/database'; // Altere o caminho se necessário
 
-const connection = new Sequelize(dbConfig);
+const connection = new Sequelize(dbConfig.url || dbConfig); // Verifica se a URL está disponível
 
 export default connection;
